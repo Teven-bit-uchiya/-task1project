12bee90c973c72ecb249668d8a0449fba4bd2298
Teven Kunene <tevenkunene@outlook.com>
2022-09-16 13:55:13 +0200

    modified to accept input from user

--- helloWorld.java
+++ helloWorld.java
@@ -1,1 +1,10 @@
-Git is Awesome 
+import java.util.Scanner;
+
+public class helloWorld{
+    public static void main(String[] args) {
+        System.out.println("Please enter your name");
+        Scanner name = new Scanner(System.in);
+        String x = name.nextLine();
+        System.out.println(x);
+    }
+}
